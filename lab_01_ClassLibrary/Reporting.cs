37428278e9b87146da0fe9f60324655e856f6eec
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-03-02 16:12:10 +0200

    lab_01 readme file and task1/task2

---
+++ lab_01_ClassLibrary/Reporting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_01_ClassLibrary
+{
+    public class Reporting
+    {
+        private Warehouse warehouse;
+
+        public Reporting(Warehouse warehouse)
+        {
+            this.warehouse = warehouse;
+        }
+
+        public void RegisterIncoming(Product product, DateTime deliveryDate)
+        {
+            warehouse.AddProduct(product, deliveryDate);
+            Console.WriteLine("Product registered");
+        }
+
+        public void RegisterOutgoing(Product product, int quantity)
+        {
+            if (product.quantity < quantity)
+            {
+                throw new InvalidOperationException("Not enough products in stock.");
+            }
+
+            product.quantity -= quantity;
+
+            Console.WriteLine($"Outgoing registered: {quantity} units of {product.name}");
+        }
+
+        public void PrintInventoryReport()
+        {
+            Console.WriteLine(warehouse.GetInventoryReport());
+        }
+
+
+    }
+}
---
+++ lab_01_ClassLibrary/Warehouse.cs
@@ -0,0 +1,26 @@
+﻿using lab_01_ClassLibrary;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+public class Warehouse
+{
+    private List<(Product Product, DateTime LastDeliveryDate)> products = new List<(Product, DateTime)>();
+
+    public void AddProduct(Product product, DateTime lastDeliveryDate)
+    {
+        products.Add((product, lastDeliveryDate));
+    }
+
+    public string GetInventoryReport()
+    {
+        var report = new StringBuilder("All products in the warehouse:\n");
+
+        foreach (var item in products)
+        {
+            report.AppendLine($"{item.Product}, Last delivery date: {item.LastDeliveryDate.ToShortDateString()}");
+        }
+
+        return report.ToString();
+    }
+}
