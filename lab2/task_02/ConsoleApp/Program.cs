10b4b581991be8965ad32867c3fb961be14dd9e7
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-03-14 16:05:44 +0200

    lab_02 task2 added diagram

---
+++ lab2/task_02/ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using FactoryClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory iPhoneFactory = new IPhoneFactory();
+            Console.WriteLine("IPhone devices:");
+            ShowDevicesInfo(iPhoneFactory);
+
+            Console.WriteLine(" ");
+
+
+            Factory xiaomiFactory = new XiaomiFactory();
+            Console.WriteLine("xiaomi devices:");
+            ShowDevicesInfo(xiaomiFactory);
+
+            Console.WriteLine(" ");
+
+
+            Factory galaxyFactory = new GalaxyFactory();
+            Console.WriteLine("Galaxy devices:");
+            ShowDevicesInfo(galaxyFactory);
+
+
+        }
+
+        static void ShowDevicesInfo(Factory factory)
+        {
+            Console.WriteLine(factory.CreateLaptop().Name());
+            Console.WriteLine(factory.CreateSmartphone().Name());
+        }
+    }
+}
