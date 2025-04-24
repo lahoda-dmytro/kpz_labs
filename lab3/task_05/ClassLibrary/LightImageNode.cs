8ad54744c9556e10d7e4a34d056530d137413b39
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-04-24 23:28:09 +0300

    lab_04 task_04

---
+++ lab3/task_05/ClassLibrary/LightImageNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class LightImageNode : LightNode
+    {
+        public string Href { get; }
+        private readonly IImageLoadStrategy _loadStrategy;
+
+        public LightImageNode(string href, IImageLoadStrategy strategy)
+        {
+            Href = href;
+            _loadStrategy = strategy;
+        }
+
+        public override string OuterHTML(int indent = 0)
+        {
+            string indentStr = new string(' ', indent);
+            string comment = _loadStrategy.Load(Href);
+            return $"{indentStr}<img src=\"{Href}\" alt=\"image\" /> <!-- {comment} -->";
+        }
+
+        public override string InnerHTML(int indent = 0) => "";
+    }
+
+}
